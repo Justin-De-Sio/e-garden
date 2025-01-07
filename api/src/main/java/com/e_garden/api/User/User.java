6591af14a9e4f017cad6c8e22195fe042be11315
rbrtthms <thomas.rob105@gmail.com>
2025-01-07 17:26:40 +0100

    userTable

---
+++ api/src/main/java/com/e_garden/api/User/User.java
@@ -0,0 +1,37 @@
+package com.e_garden.api.User;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Getter
+    @Column(nullable = true, length = 255)
+    private String name;
+
+    @Setter
+    @Getter
+    @Column(nullable = true, length = 255)
+    private String surname;
+
+    @Setter
+    @Getter
+    @Column(nullable = false, length = 255)
+    private String email;
+
+    @Setter
+    @Getter
+    @Column(nullable = true , length = 2048)
+    private String password;
+}
