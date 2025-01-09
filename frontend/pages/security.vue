278825d0fe57c0418ef3cc7a27024b86d60c3135
Gabin-Crn <gabincornaire@outlook.fr>
2025-01-09 15:04:27 +0100

    resolve navbar component

--- frontend/pages/security.vue
+++ frontend/pages/security.vue
@@ -1,15 +1,13 @@
 
+<template>
+  <navbar></navbar>
+</template>
+
 
 <script setup lang="ts">
 
 </script>
 
-<template>
-<h1>
-  security
-</h1>
-  <navbar> </navbar>
-</template>
 
 <style scoped>
 
