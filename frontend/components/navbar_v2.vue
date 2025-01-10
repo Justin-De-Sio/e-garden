d57ab6a12242d5f600de8d0e3e4a2332684d375c
Gabin-Crn <gabincornaire@outlook.fr>
2025-01-11 00:17:45 +0100

    navbar done

--- frontend/components/navbar_v2.vue
+++ frontend/components/navbar_v2.vue
@@ -1,28 +1,51 @@
 <template>
-    <nav>
+    <nav :class="{expanded : IsExpanded}">
       <div class="placement_icon_navbar">
         <div class="logo_placement">
-          <NuxtLink to="/">
+          <NuxtLink to="/login" class="logo-link">
             <img src="../assets/logo.png" alt="Logo" width="80" height="auto" />
           </NuxtLink>
         </div>
-        <div class="expand_navbar">
+        <div class="expand_navbar" @click="toggleMenu">
           <div class="circle_around_angle">
-            <img src="../assets/angle.png" alt="Angle" width="25px" height="25px" />
+              <img :class="{ rotated: IsExpanded }" src="../assets/angle.png" alt="Angle" width="25px" height="25px" />
           </div>
         </div>
-        <div class="project_placement">
-          <NuxtLink to="/">
-            <img src="../assets/applications.png" alt="Application" />
+        <div class="project_placement" :class="{expanded: IsExpanded}">
+          <NuxtLink to="/" >
+            <div class="main_wrapper" :class="{expanded: IsExpanded}">
+              <div class="wrapper_img">
+                <img src="../assets/applications.png" alt="Application" />
+              </div>
+              <h4>Dashboard</h4>
+            </div>
+          
           </NuxtLink>
           <NuxtLink to="/">
-            <img src="../assets/camera.png" alt="Camera" />
+            <div class="main_wrapper" :class="{expanded: IsExpanded}">
+              <div class="wrapper_img">
+                <img src="../assets/camera.png" alt="Camera" />
+              </div>
+              <h4>Sécurité</h4>
+            </div>
+            
           </NuxtLink>
           <NuxtLink to="/">
-            <img src="../assets/capteur.png" alt="Capteur" />
+            <div class="main_wrapper" :class="{expanded: IsExpanded}">
+              <div class="wrapper_img">
+                <img src="../assets/capteur.png" alt="Capteur" />
+              </div>
+              <h4>Capteurs</h4>
+            </div>
+            
           </NuxtLink>
           <NuxtLink to="/">
-            <img src="../assets/gestion.png" alt="Gestion" />
+            <div class="main_wrapper" :class="{expanded: IsExpanded}">
+              <div class="wrapper_img">
+                <img src="../assets/gestion.png" alt="Gestion" />
+              </div>
+              <h4>Gestionnaire</h4>
+            </div>
           </NuxtLink>
         </div>
       </div>
@@ -30,30 +53,56 @@
   </template>
   
   <script>
-  export default {};
+  export default {
+    data(){
+      return {
+        IsExpanded: false,
+      };
+    },
+    methods : {
+      toggleMenu(){
+        this.IsExpanded = !this.IsExpanded;
+      },
+    },
+  };
   </script>
   
   <style lang="css" scoped>
   nav {
     display: flex;
     justify-content: center;
+    padding: 0 30px;
     width: 125px;
     height: 100vh;
     background-color: #151b11;
     position: relative;
+    transition: width 0.3s ease;
   }
   
+  nav.expanded{
+    width: 270px;
+
+  }
   .logo_placement {
     margin-top: 5em;
     display: flex;
     justify-content: center;
+    align-items: center;
   }
   
   .project_placement {
     display: grid;
     grid-template-columns: 1fr;
-    gap: 3em;
+    gap: 1.5em;
     justify-items: center;
+    margin-top: 5em;
+  }
+
+  .project_placement.expanded{
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 1.5em;
+    justify-items: start;
     margin-top: 5em;
   }
   
@@ -61,11 +110,15 @@
     width: 25px;
     height: auto;
     cursor: pointer;
-    transition: transform 0.2s ease;
   }
   
-  .project_placement img:hover {
-    transform: scale(1.2);
+  
+
+  .wrapper_img{
+    display: flex;
+    justify-content: center;
+    padding: 18px;
+
   }
   
   .expand_navbar {
@@ -91,7 +144,53 @@
     display: block;
   }
 
+  .main_wrapper{
+    display: flex;
+    flex-direction: row;
+    justify-items: center;
+    align-items: center;
+    border-radius: 15px;
+  }
+
+  .main_wrapper.expanded{
+    display: flex;
+    flex-direction: row;
+    align-items: left;
+    justify-content: flex-start;
+    padding-right: 2em;
+  }
+
+  h4{
+    display: none;
+    color: white;
+    text-decoration: none;
+    font-family: "Gilroy-Medium";
+  }
+
+  nav.expanded h4 {
+  display: block;
+  margin-left: 20px;
+  }
 
 
+  a{
+  text-decoration: none;
+  color: inherit;
+  width: 100%;
+  }
+
+  a.logo-link {
+  width: auto; 
+  }
+
+  .circle_around_angle img.rotated {
+  transform: rotate(90deg);
+  }
+
+  .main_wrapper:hover{
+    background-color: #95BD75;
+  
+
+  }
   </style>
   
