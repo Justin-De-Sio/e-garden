c69d0984b195f44ec9b7e82b870449c681780f84
augustinmeaudre <118811814+augustinmeaudre@users.noreply.github.com>
2025-01-13 12:11:28 +0100

    indicatorV1 ok images en dynamique non prise en compte vérifier marge à faire

--- frontend/pages/security.vue
+++ frontend/pages/security.vue
@@ -1,14 +1,50 @@
 
 <template>
+
+  <div id="app">
+    <CustomComponent
+        iconPath="/assets/conge.png"
+        iconBackgroundColor="#95BD75"
+        title="XX passages"
+        subtitle="Nombre de passage"
+    />
+  </div>
+
+  <div id="app">
+    <CustomComponent
+        iconClass="@/assets/disque.png"
+        iconBackgroundColor="#457B9D54"
+        title="XX% utilisé"
+        subtitle="stockage disque dur"
+    />
+  </div>
+
+  <div id="app">
+    <CustomComponent
+        iconClass="@/assets/attention.png"
+        iconBackgroundColor="#DA555280"
+        title="XX absences de CR"
+        subtitle="Alertes"
+    />
+  </div>
+  <!--
   <navbar></navbar>
   <div class="wrapper_notification_component">
-    <notification.vue></notification.vue>
-  </div>
+    <notification></notification>
+  </div>-->
+
 </template>
 
 
-<script setup lang="ts">
+<script>
+import CustomComponent from '@/components/indicator.vue';
 
+export default {
+  name: 'App',
+  components: {
+    CustomComponent
+  }
+}
 </script>
 
 
@@ -18,4 +54,6 @@
   justify-content: center;
   align-items: center;
 }
+
+
 </style>
