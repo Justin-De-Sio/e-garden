ab618ec0459e63a1a67876d9b8dee2979676bb3c
julien <jparquet78@gmail.com>
2025-01-13 08:34:51 +0100

    push de la partie events

---
+++ api/src/main/java/com/e_garden/api/Events/Events.java
@@ -0,0 +1,21 @@
+package com.e_garden.api.Events;
+
+import com.e_garden.api.Events.Events;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "events")
+@Getter
+@Setter
+
+public class Events {
+
+
+
+
+
+}
---
+++ api/src/main/java/com/e_garden/api/Events/EventsController.java
@@ -0,0 +1,4 @@
+package com.e_garden.api.Events;
+
+public class EventsController {
+}
