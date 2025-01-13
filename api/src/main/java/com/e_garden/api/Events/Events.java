ec5164a64380df4816f9eb7435729393aaab47cd
rbrtthms <thomas.rob105@gmail.com>
2025-01-13 16:40:38 +0100

    events

--- api/src/main/java/com/e_garden/api/Events/Events.java
+++ api/src/main/java/com/e_garden/api/Events/Events.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-
 @Entity
 @Table(name = "events")
 @Setter
@@ -16,17 +15,56 @@ public class Events {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = true)
     private String description;
+
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
-    // Constructeurs, getters, setters
-    public Events() {}
-
-    public Events(String title, String description, LocalDateTime createdAt) {
-        this.title = title;
-        this.description = description;
-        this.createdAt = createdAt;
+    public Events() {
+        this.title = "";
+        this.createdAt = LocalDateTime.now();
     }
 
+    public Events(String title, String description) {
+        this();
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
