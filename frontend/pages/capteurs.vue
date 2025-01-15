c10ceca4ee959ed66ce93c5bb7aa0307f415084f
YolaineDrouart <96011211+YolaineDrouart@users.noreply.github.com>
2025-01-15 16:25:18 +0100

    Create capteurs.vue
    base de la page Capteurs PPE

---
+++ frontend/pages/capteurs.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+      <navbar />
+      <main class="main-content">
+        <header class="header">
+          <h1>Section capteur</h1>
+          <p>Tout vos capteurs réunis, ici</p>
+        </header>
+        <section class="content">
+          <!-- Contenu principal ici -->
+        </section>
+      </main>
+    </div>
+  </template>
+  
+  <script>
+  import Navbar from "./components/navbar.vue"; 
+  
+  export default {
+    name: "App",
+    components: {
+      Navbar,
+    },
+  };
+  </script>
