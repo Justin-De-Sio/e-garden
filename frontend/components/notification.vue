dc5c5fa6949b8109fc754e39a83bdbf6ae7013dc
augustinmeaudre <118811814+augustinmeaudre@users.noreply.github.com>
2025-01-15 08:40:13 +0100

    centre notif V1 début requete

--- frontend/components/notification.vue
+++ frontend/components/notification.vue
@@ -4,7 +4,21 @@
       <img src="@/assets/cloche.png" alt="Button Image" class="button-image" />
     </button>
     <div v-if="showRectangle" class="rectangle">
+      <div class="icon-group">
+        <button class="arrow-button" @click="fetchPreviousPage">
+          <img src="@/assets/up-arrow.png" alt="Up Arrow" class="arrow-image" />
+        </button>
+        <button class="arrow-button" @click="fetchNextPage">
+          <img src="@/assets/down-arrow.png" alt="Down Arrow" class="arrow-image" />
+        </button>
+      </div>
       <span class="rectangle-title">Notifications</span>
+      <hr class="separator-line" />
+      <ul>
+        <li v-for="notification in notifications" :key="notification.id">
+          {{ notification.message }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -15,17 +29,39 @@ export default {
   data() {
     return {
       showRectangle: false,
+      notifications: [],
+      currentPage: 1,
     };
   },
   methods: {
     toggleRectangle() {
       this.showRectangle = !this.showRectangle;
+      if (this.showRectangle) {
+        this.fetchNotifications();
+      }
+    },
+    async fetchNotifications() {
+      const response = await fetch(`http://localhost:8080/event/paginated?page=${this.currentPage}&size=5`);
+      const data = await response.json();
+      this.notifications = data.content;
+    },
+    fetchNextPage() {
+      this.currentPage += 1;
+      this.fetchNotifications();
+    },
+    fetchPreviousPage() {
+      if (this.currentPage > 1) {
+        this.currentPage -= 1;
+        this.fetchNotifications();
+      }
     },
   },
 };
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Gilroy:wght@500&display=swap');
+
 .button-container {
   position: relative;
 }
@@ -61,16 +97,46 @@ export default {
   padding: 10px; /* Ajout de padding pour le texte */
   box-sizing: border-box;
   margin-bottom: 5px;
-
   margin-right: 5px;
+  border-radius: 15px; /* Ajout du radius de 15px */
 }
 
 .rectangle-title {
   position: absolute;
-  top: 10px;
-  left: 10px;
+  top: 15px; /* Décalage de 5px vers le bas */
+  left: 20px; /* Décalage de 10px vers la droite */
   font-size: 16px;
-  font-weight: bold;
+  font-family: 'Gilroy', sans-serif; /* Application de la police Gilroy-Medium */
+}
 
+.icon-group {
+  display: flex;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
+
+.arrow-button {
+  width: 30px;
+  height: 30px;
+  background-color: #95BD75; /* Fond vert derrière l'icône */
+  border: 1px solid #ccc;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  margin-left: 5px;
+}
+
+.arrow-image {
+  width: 16px;
+  height: 16px;
+}
+
+.separator-line {
+  margin-top: 40px; /* Espace entre le titre et la ligne */
+  border: none;
+  border-top: 1px solid #ccc;
 }
 </style>
