f736c386b70523e3efd9d858995d3f8b970aee2b
Gabin-Crn <gabincornaire@outlook.fr>
2025-01-16 17:58:56 +0100

    Cross changemnent back

---
+++ frontend/services/notification.js
@@ -0,0 +1,27 @@
+export async function Notification(page, size) {    
+    const url = `http://localhost:8080/api/event/1`;    
+    //const url = `http://localhost:8080/api/event/paginated?page=${page}&size=${size}`;    
+    try {        
+        const sessionCookie = useCookie("session");        
+        const token = sessionCookie.value;               
+         const response = await fetch(url, {           
+             method: 'GET',           
+              headers: {                
+                "Content-Type": "application/json", 
+                "Access-Control-Allow-Origin": "*", 
+                'Access-Control-Allow-Headers': '*',               
+                "Authorization": `Bearer ${token}`            
+            }        
+        });        
+        if (!response.ok) {            
+            throw new Error(`HTTP error! status: ${response.status}`);        
+        }        
+        const data = await response.json();        
+        console.log(data);        
+        return data;    
+    
+    } catch (error) {        
+        console.error('Error fetching data:', error);        
+        return null;
+         // Retourner null en cas d'erreur    
+         }}
