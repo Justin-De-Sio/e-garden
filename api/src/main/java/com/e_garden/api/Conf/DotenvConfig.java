9c7ad2a3a8ed72510b2b8b9e37c27a0a214d29d2
rbrtthms <thomas.rob105@gmail.com>
2025-01-17 11:12:30 +0100

    doc DotEnv

--- api/src/main/java/com/e_garden/api/Conf/DotenvConfig.java
+++ api/src/main/java/com/e_garden/api/Conf/DotenvConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DotenvConfig {
 
+    /**
+     * Cette fonction permet de charger le fichier avec les variables d'environnements.
+     * Elle est lancé automatiquement par Spring Boot avec l'annotation @Bean.
+     * En fonction du projet directory dans votre machine local ou du serveur, le .directory('PATH') peut être changé.
+     * @return elle retourne un object Dotenv spécifique.
+     */
     @Bean
     public static Dotenv load() {
         Dotenv dotenv = Dotenv.configure().directory("api/").filename(".env").load();
