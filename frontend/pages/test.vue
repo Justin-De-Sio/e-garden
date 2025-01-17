6fb7ffa73c5a3050c9e4c0eb8a1e255f866f94b5
Gabin-Crn <gabincornaire@outlook.fr>
2025-01-17 17:37:53 +0100

    HoverCard in CR in Progress

--- frontend/pages/test.vue
+++ frontend/pages/test.vue
@@ -1,32 +1,123 @@
 <template>
-    <div>
-      <h1>Événement</h1>
-      <div v-if="event">{{ event }}</div>
-      <div v-else>Chargement...</div>
-    </div>
+        <div class="CardProfilHover">
+            <div class="header_card">
+                <div class="iconCoverfit">
+                    <img src="~/assets/profil_cr.jpg" alt="" width="20px" height="auto"/>
+                </div>
+                <div class="identity">
+                    <div class="name_surname">
+                        <h3>Pierre</h3>
+                        <h3>Courtois</h3>
+                    </div>
+                    <h4>ING 4 - Grp03 </h4>
+                </div>
+            </div>
+            <div class="metrics">
+                <div class="intern_metrics">
+                    <h5>Rôle :</h5>
+                    <h6>Utilisateur</h6>
+                </div>
+                <div class="intern_metrics">
+                    <h5>Rôle :</h5>
+                    <h6>Utilisateur</h6>
+                </div>
+            </div>
+        </div>
   </template>
   
   <script setup>
-  import { ref, onMounted } from 'vue';
-  
-  const event = ref(null);
-  const sessionCookie = useCookie("session");
-    const token = sessionCookie.value;
-    const errorMessage = ref('');
-
-    onMounted(async () => {
-    try {
-        const data = await $fetch('/api/report/1', {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-        });
-        event.value = data;
-    } catch (error) {
-        console.error('Erreur lors de la requête API :', error);
-        errorMessage.value = 'Impossible de charger l’événement. Veuillez réessayer plus tard.';
-    }
-    });
 
   </script>
+
+<style>
+body{
+    background-color: beige;
+}
+
+h3{
+    color: black;
+    font-family: "Gilroy-Medium", sans-serif;
+}
+
+h4{
+    color: #7B7B7B;
+    font-family: "Gilroy-Regular";
+    font-size: 0.8rem;
+}
+
+
+h5{
+    color: #7B7B7B;
+    font-family: "Gilroy-Regular";
+}
+
+h6{
+    color: black;
+    font-family: "Gilroy-Regular";
+    font-size: 0.8rem;
+}
+
+.CardProfilHover{
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    padding: 2rem;
+    width: 20rem;
+    height: auto;
+    border-radius: 1rem;
+    background-color: white;
+    transition: box-shadow 0.3s ease-in-out;
+
+}
+
+.CardProfilHover:hover {
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2); /* Ombre uniquement en bas */
+}
+
+.iconCoverfit {
+    display: flex;
+
+    border: 1px solid black;
+    width: 2.5rem;
+    border-radius: 50%;
+    overflow: hidden;
+    justify-content: center;
+    align-items: center;
+    height: 2.5rem;
+  }
+  .iconCoverfit img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .header_card{
+    display: flex;
+    flex-direction: row;
+  }
+
+  .identity{
+    margin-left: 1rem;
+  }
+
+  .name_surname{
+    display: flex;
+    flex-direction: row;
+    gap: 0.5rem;
+  }
+
+  .metrics{
+    margin-top: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.6rem;
+  }
+
+  .intern_metrics{
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+  }
+
+</style>
   
