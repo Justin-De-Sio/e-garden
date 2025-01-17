b3ef84370dffbe629542e85312aaa4bb151cb9a3
rbrtthms <thomas.rob105@gmail.com>
2025-01-17 13:33:59 +0100

    doc loadUser

--- api/src/main/java/com/e_garden/api/User/MyUserDetailsService.java
+++ api/src/main/java/com/e_garden/api/User/MyUserDetailsService.java
@@ -17,14 +17,17 @@ public class MyUserDetailsService implements UserDetailsService {
     }
 
     /**
-     * @param username
-     * @return
-     * @throws UsernameNotFoundException
+     * Méthode permettant de charger un object utilisateur principal.
+     * UserPrincipal est renseigné à partir d'un utilisateur trouvé en DB avec un email.
+     *
+     * @param username email de l'utilisateur
+     * @return l'UserPrincipal correspondant à l'email
+     * @throws UsernameNotFoundException Exception en cas d'absence de l'utilisateur
      */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(username);
-        if(user == null){
+        if (user == null) {
             System.out.println("User Not Found");
             throw new UsernameNotFoundException("user not found");
         }
