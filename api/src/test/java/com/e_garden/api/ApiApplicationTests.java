a0100099992ffc6b0a9f2434b1840bf8d796e4ae
rbrtthms <thomas.rob105@gmail.com>
2025-01-20 10:02:35 +0100

    remove detenvconf for test

--- api/src/test/java/com/e_garden/api/ApiApplicationTests.java
+++ api/src/test/java/com/e_garden/api/ApiApplicationTests.java
@@ -2,15 +2,9 @@ package com.e_garden.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootTest
 class ApiApplicationTests {
-
-	@Configuration
-	static class env {
-
-	}
 
 	@Test
 	void contextLoads() {
