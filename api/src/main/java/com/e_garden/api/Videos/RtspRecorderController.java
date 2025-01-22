677c15670c031ed25f38340d210cfa5d09e8f576
Justin <desio.j@live.fr>
2025-01-22 17:20:21 +0100

    record in db

--- api/src/main/java/com/e_garden/api/Videos/RtspRecorderController.java
+++ api/src/main/java/com/e_garden/api/Videos/RtspRecorderController.java
@@ -1,11 +1,13 @@
 package com.e_garden.api.Videos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.time.LocalDateTime;
+import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -16,9 +18,15 @@ import java.util.concurrent.TimeUnit;
 public class RtspRecorderController {
 
     private static final String OUTPUT_DIRECTORY = "rtsp";
-    private static final String OUTPUT_FILENAME = "record.mp4";
     private static final String RTSP_URL_ENV = "RTSP_URL";
     private final ExecutorService executor = Executors.newCachedThreadPool();
+
+    private final VideoService videoService;
+
+    @Autowired
+    public RtspRecorderController(VideoService videoService) {
+        this.videoService = videoService;
+    }
 
     @PostMapping("/record")
     public Response recordRtspStream(@RequestParam(required = false, defaultValue = "5") int duration) {
@@ -27,7 +35,10 @@ public class RtspRecorderController {
             return new Response(false, "RTSP_URL environment variable is not set or empty.");
         }
 
-        String outputFile = OUTPUT_DIRECTORY + File.separator + LocalDateTime.now().toString() +  OUTPUT_FILENAME;
+        String uuid = UUID.randomUUID().toString();
+        String fileName = uuid + ".mp4";
+        String filePath = OUTPUT_DIRECTORY + File.separator + fileName;
+
 
         // Crée le répertoire de sortie si nécessaire
         File outputDir = new File(OUTPUT_DIRECTORY);
@@ -40,16 +51,16 @@ public class RtspRecorderController {
         // Commande FFmpeg avec durée spécifiée
         String command = String.format(
                 "ffmpeg -rtsp_transport tcp -timeout 10000000 -analyzeduration 100000000 -probesize 50000000 -i %s -t %d -s 2560x1920 -r 20 -c:v libx264 -preset ultrafast -c:a aac -b:a 128k -f mp4 %s",
-                rtspUrl, duration, outputFile
+                rtspUrl, duration, filePath
         );
 
-        executor.submit(() -> runRecordingProcess(command, outputFile));
+        executor.submit(() -> runRecordingProcess(command, fileName, outputDir.getAbsolutePath()));
 
         return new Response(true, "Recording started for " + duration + " seconds.");
     }
 
     @Async
-    private void runRecordingProcess(String command, String outputFile) {
+    private void runRecordingProcess(String command, String fileName, String filePath) {
         try {
             ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
             processBuilder.redirectErrorStream(true);
@@ -72,11 +83,15 @@ public class RtspRecorderController {
             if (!finished) {
                 process.destroy(); // Forcer l'arrêt si le timeout est atteint
                 System.err.println("Recording process timed out.");
+
             }
 
             int exitCode = process.exitValue();
             if (exitCode == 0) {
-                System.out.println("Recording completed successfully! Saved to: " + outputFile);
+                System.out.println("Recording completed successfully! Saved to: " + fileName);
+                Video video = new Video(fileName, filePath);
+                videoService.saveVideo(video);
+                
             } else {
                 System.err.println("Recording failed with exit code: " + exitCode);
             }
