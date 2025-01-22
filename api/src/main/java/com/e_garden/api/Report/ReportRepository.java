ac5705c6ac2770f604a6bf399c62217259dcfef6
rbrtthms <thomas.rob105@gmail.com>
2025-01-22 15:17:41 +0100

    get all reports

--- api/src/main/java/com/e_garden/api/Report/ReportRepository.java
+++ api/src/main/java/com/e_garden/api/Report/ReportRepository.java
@@ -1,6 +1,7 @@
 package com.e_garden.api.Report;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
-    Page<Report> findAllByValidatedOrderByReportDateAsc(boolean validated, Pageable pageable);
+    //Page<Report> findAllByValidatedOrderByReportDateAsc(boolean validated, Pageable pageable);
     Integer countAllByValidatedAndReportDateGreaterThan(boolean validated, LocalDateTime reportDate);
+    Page<Report> findAllByOrderByReportDateAsc(PageRequest of);
 }
