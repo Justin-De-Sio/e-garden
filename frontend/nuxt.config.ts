b4a015323d88bde0ce4297dc897e87e41ad9d82d
Gabin-Crn <gabincornaire@outlook.fr>
2025-01-22 16:38:30 +0100

    edit login avec Uform

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,13 +4,16 @@ export default defineNuxtConfig({
   css: [
     '/public/assets/css/reset.css', // Inclure le fichier CSS global
   ],
-  
-
+  modules:[
+    '@nuxt/ui'
+  ],
   runtimeConfig: {
     public: {
       apibase: process.env.API_URL || 'thomas',
     },
   },
-
+  colorMode:{
+    preference: "light",
+  },
   compatibilityDate: '2025-01-20',
 });
