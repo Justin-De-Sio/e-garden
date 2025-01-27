1264b1bfcaa2479c3367ac2e0b6fc7880dfbfdeb
Gabin-Crn <gabincornaire@outlook.fr>
2025-01-27 15:05:02 +0100

    install package calendar

---
+++ frontend/components/calendar.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="wrapper">
+    <UPopover :popper="{ placement: 'bottom-start' }">
+        <UButton icon="i-heroicons-calendar-days-20-solid" :label="format(date, 'd MMM, yyy')" />
+
+        <template #panel="{ close }">
+        <DatePicker v-model="date" is-required @close="close" />
+        </template>
+    </UPopover>
+</div>
+</template>
+
+<script setup lang="ts">
+import { format } from 'date-fns'
+
+const date = ref(new Date())
+</script>
+
+<style></style>
---
+++ frontend/pages/test.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <calendar></calendar>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import calendar from '~/components/calendar.vue'
+</script>
+
+<style scoped>
+
+</style>
