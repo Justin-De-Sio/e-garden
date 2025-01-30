f5198e3b5698ce29662c23c3e468a46de0d7778c
augustinmeaudre <118811814+augustinmeaudre@users.noreply.github.com>
2025-01-30 14:10:56 +0100

    Calendrier terminé

--- frontend/pages/ppe1.vue
+++ frontend/pages/ppe1.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-
+import NavbarVertical from "~/components/navbar_vertical.vue"
+import calendar from "~/components/calendar.vue";
 </script>
 
 <template>
+  <navbar-vertical/>
 <h1>ppe1</h1>
+  <calendar/>
 </template>
 
 <style scoped>
