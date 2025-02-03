881b3ebcb2e286d5a3ef2f0dd5a89527416e68e6
rbrtthms <thomas.rob105@gmail.com>
2025-02-03 09:22:28 +0100

    docs getToken

--- frontend/services/getToken.ts
+++ frontend/services/getToken.ts
@@ -1,5 +1,14 @@
 import jwtDecode from "jwt-decode";
 
+/**
+ * Fonction pour récupérer le token de l'utilisateur.
+ * Le token contient plusieurs informations :
+ * - le "role" de l'utilisateur ;
+ * - le "sub", email, de l'utilisateur ;
+ * - le "iat", date de création, du token :
+ * - le "exp", date d'expiration, du token.
+ * @returns {object Token}
+ */
 export function getToken() {
     let sessionCookie;
     try {
