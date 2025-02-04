07497fcba29f287538e82db3c5ac804dee044fbb
rbrtthms <thomas.rob105@gmail.com>
2025-02-04 16:09:44 +0100

    remove old import

--- api/src/main/java/com/e_garden/api/door/DoorController.java
+++ api/src/main/java/com/e_garden/api/door/DoorController.java
@@ -3,7 +3,6 @@ package com.e_garden.api.door;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
