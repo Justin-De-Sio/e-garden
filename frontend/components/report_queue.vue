e7255901166d4eb2d13674d7485f6fe2fcaf7429
rbrtthms <thomas.rob105@gmail.com>
2025-02-05 15:17:13 +0100

    charger plus

--- frontend/components/report_queue.vue
+++ frontend/components/report_queue.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="feed">
     <div v-for="post in reports" :key="post.id" class="post">
-      <p class="date">{{ post.reportDate }}</p>
+      <p class="date">
+        <NuxtTime :datetime="post.reportDate" day="2-digit" month="2-digit" year="numeric" hour="2-digit"
+                                minute="2-digit"/>
+      </p>
       <p class="content">{{ post.content }}</p>
     </div>
-    <div>
-      <UButton @click="LoadMore">Charger plus...</UButton>
+    <div v-if="(numPages + 1) >= numTotalPages">
+      <UButton @click="requetRepport">Charger plus...</UButton>
     </div>
   </div>
 </template>
@@ -19,15 +22,11 @@ import { onMounted, ref } from "vue";
 const api = new callAPIServices();
 const reports = ref<Reports[]>([]);
 const numPages = ref<number>(0);
+const numTotalPages = ref<number>(0);
 
 onMounted(async () => {
   await requetRepport();
 });
-
-async function LoadMore() {
-  numPages.value++;
-  await requetRepport();
-}
 
 async function requetRepport() {
   try {
@@ -35,6 +34,8 @@ async function requetRepport() {
     console.log(response.content);
     // Mise à jour de reports en concaténant le contenu
     reports.value = reports.value.concat(response.content as Reports[]);
+    numTotalPages.value = response.totalPages;
+    numPages.value++;
     console.log(reports.value);
   } catch (error) {
     console.error('Erreur lors de la récupération des données :', error);
